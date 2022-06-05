19d84456d52a9e89330ce1a401fd3f70a3343455
Elizaveta <lisasorokina990@gmail.com>
2022-06-04 21:29:14 -0400

    Implemented Loan Calculator

--- loanCalculator/Source.cpp
+++ loanCalculator/Source.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <cmath>
 using namespace std;
 
 int main()
 {
 	
-	
+	const int MONTHS_IN_YEAR = 12;
 	string name, institution, date;
 	int years;
 	double loanAmt, intRate;
@@ -18,8 +19,7 @@ int main()
 	cout << "\nPlease enter the following information" << endl
 		<< "\tBorrower's name: ";
 	getline(cin, name);
-	cout << "\tLoaning institution: ";
-	cin.ignore();
+	cout << "\tLoaning institution: ";	
 	getline(cin, institution);
 	cout << "\tLoan amount: ";
 	cin >> loanAmt;
@@ -29,7 +29,27 @@ int main()
 	cin >> years;
 	cout << "\tDate of Report: ";
 	cin >> date;
+	double monthlyIntRate = intRate / MONTHS_IN_YEAR;
+	int numOfPaymnets = years * MONTHS_IN_YEAR;
+	double rateMultiplier = pow(1+monthlyIntRate/100, numOfPaymnets);
+	double monthlyPay = monthlyIntRate*rateMultiplier*loanAmt/(rateMultiplier-1)/100;
 
+	double totAmntPay = monthlyPay*numOfPaymnets;
+	double totInterest = totAmntPay-loanAmt;
 	
+	cout << "--------------------------------------"<<endl
+	 << "Loan Payment Summary Report"<< endl
+	 << endl <<left << setw(30) << institution << name<< endl
+	 << "Annual Interest Rate: "<< intRate << "%"<<endl
+	 << "Date: "<< date <<endl
+	 << endl
+	 << left << setw(30) <<	"Loan Amount: " << "$ " << loanAmt << endl
+	 << left << setw(30) << "Monthly Interest Rate: " << monthlyIntRate<< "%"<<endl
+	 << left << setw(30) << "Number of Payments: " << numOfPaymnets<< endl
+	 << left << setw(30) << "Monthly Payment: " << "$ " << monthlyPay << endl
+	 << endl
+	 << left << setw(30) << "Total Amount to Pay: " << "$ " << totAmntPay << endl
+	 << left << setw(30) << "Total Interest: " << "$ " << totInterest << endl;
+	 return  0;	
 	
 }
